3638930549f9978fc81a04ad9232bd00d6db306e
GeorgeFreeman <thenewsit@gmail.com>
2018-08-24 16:34:16 +0500

    fix

--- src/db/MultilingualTrait.php
+++ src/db/MultilingualTrait.php
@@ -12,32 +12,6 @@ use Yii;
 trait MultilingualTrait
 {
     /**
-     * @var string the name of the lang field of the translation table. Default to 'language'.
-     */
-    public $languageField = 'language';
-
-    /**
-     * Scope for querying by languages.
-     *
-     * @param $language
-     * @return $this
-     */
-    public function localized($language = null)
-    {
-        if (!$language) {
-            $language = Yii::$app->language;
-        }
-
-        if (!isset($this->with['translations'])) {
-            $this->with(['translation' => function ($query) use ($language) {
-                $query->where([$this->languageField => $language]);
-            }]);
-        }
-
-        return $this;
-    }
-
-    /**
      * Scope for querying by all languages
      * @return $this
      */
